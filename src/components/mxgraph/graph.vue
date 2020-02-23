a858021104f49043ba5b146d4fc6ba493b58f0b2
欧阳明择 <enforceway@163.com>
2020-02-23 17:11:30 +0800

    测试分支添加mxgraph的测试集成

--- src/components/mxgraph/graph.vue
+++ src/components/mxgraph/graph.vue
@@ -1,22 +1,63 @@
 <template>
   <div class="graph-jar">
+    <p><button @click="triggerExecute">执行</button></p>
     <div id="container"></div>
   </div>
 </template>
 
 <script>
-import { graph, editor, mxRubberband, mxGraphHandler } from '@/constants/graph'
-
+import { graph, editor, mxRubberband, mxGraphHandler, mxEvent, mxConstants, mxUtils } from '@/constants/graph'
+// Vue.extends(bean, mxGraph)
 export default {
   components: {
     // graph: mxgraphModel
   },
+  methods: {
+    triggerExecute () {
+      var cellModel = graph.getModel().getCell(5)
+      var previous = cellModel.getStyle
+      cellModel.getStyle = function () {
+        var cell = this
+        if (cell != null) {
+          var style = previous.apply(this, arguments)
+          if (this.isEdge(cell)) {
+            var target = this.getTerminal(cell, false)
+
+            if (target != null) {
+              var state = graph.getView().getState(target)
+              var targetStyle = (state != null) ? state.style : graph.getCellStyle(target)
+              var fill = mxUtils.getValue(targetStyle, mxConstants.STYLE_FILLCOLOR)
+              if (fill != null) {
+                style += ';strokeColor=' + fill
+              }
+            }
+          } else if (this.isVertex(cell)) {
+            var geometry = this.getGeometry(cell)
+            if (geometry != null &&
+              geometry.width >= 80) {
+              style += ';fillColor=green'
+            }
+          }
+          return style
+        }
+        return null
+      }
+      graph.refresh()
+    }
+  },
   mounted () {
+    // mxGraph.call(this)
+    console.log(this.createHandlers)
     this.$nextTick(() => {
       let container = document.querySelector('#container')
       // var graph = editor.createGraph()
       editor.setGraphContainer(container)
       graph.getView().updateStyle = true
+      graph.setConnectable(true)
+      graph.autoScroll = false
+      graph.setTooltips(false)
+      graph.setEnabled(true)
+
       // Enables guides
       mxGraphHandler.prototype.guidesEnabled = true
       /* eslint-disable */
@@ -26,26 +67,45 @@ export default {
       var parent = graph.getDefaultParent()
       // Adds cells to the model in a single step
       graph.getModel().beginUpdate()
+      /* 捆绑事件 */
+      // graph.fireMouseEvent = function(name, mxEvt) {
+      //   if(name === mxEvent.MOUSE_DOWN) {
+      //     var e = mxEvt.evt.getProperty('event'); // mouse event
+      //     var cell = mxEvt.getProperty('cell'); // cell may be null
+      //     debugger
+      //     console.log(cell)
+      //   }
+      // };
+      graph.getModel().addListener(mxEvent.CHANGE, function(sender, evt) {
+        debugger
+        // console.log('graph CHANGE triggered')
+      });
+      
+      graph.addListener(mxEvent.CLICK, function(sender, evt) {
+        var e = evt.getProperty('event'); // mouse event
+        var cell = evt.getProperty('cell'); // cell may be null
+        console.log('graph click triggered')
+        if (cell != null) {
+          // Do something useful with cell and consume the event
+          evt.consume();
+        }
+      });
+      var text = graph.getModel().getCell(3);
+      // debugger
       try {
         var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30, 'fillColor=green')
         var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30, 'fillColor=blue')
         graph.insertVertex(parent, null, 'World!', 20, 150, 80, 30, 'fillColor=red')
         graph.insertEdge(parent, null, 'Connect', v1, v2, 'perimeterSpacing=4;strokeWidth=4;labelBackgroundColor=white;fontStyle=1')
-
         // var e1 = graph.insertEdge(parent, null, '', v1, v2)
       } finally {
         // Updates the display
         graph.getModel().endUpdate()
+        // mxEvent.addListener(text, 'keydown', function (evt) {
+        //   console.log('onkeydown')
+        // }); 
       }
     })
-    // MxgraphModel(document.querySelector('#container'))
-    // console.log(window.mxGraph)
-  },
-  // name: 'HelloWorld',
-  methods: {
-    clickToChanage () {
-      this.test = !this.test
-    }
   },
   props: {
     msg: String
--- src/constants/graph.js
+++ src/constants/graph.js
@@ -2,17 +2,41 @@ let mxgraph = require('mxgraph')
 let mxgraphModel = mxgraph({
   // mxResourceExtension: '',
   // mxForceIncludes: '',
-  // mxLoadResources: '',
+  // mxLoadResources: true,
+  // mxDefaultLanguage : '',
   mxImageBasePath: './mxgraph/images',
   mxBasePath: './mxgraph'
 })
 let MxEditor = mxgraphModel.mxEditor
 let geditor = new MxEditor()
-// export default mxgraphModel
+
+/* 图形中新增cell的事件捕捉 */
+geditor.graph.addListener(mxgraphModel.mxEvent.CELLS_ADDED, function (graph, evt, arg1, arg2) {
+  // console.log(evt.properties.cells[0])
+})
+
+/* 图形中移除cell的事件捕捉 */
+geditor.graph.addListener(mxgraphModel.mxEvent.CELLS_REMOVED, function (graph, evt, arg1, arg2) {
+  // console.log(evt.properties.cells)
+})
+
+/* 图形中移动cell的事件捕捉 */
+geditor.graph.addListener(mxgraphModel.mxEvent.CELLS_MOVED, function (graph, evt, arg1, arg2) {
+  // console.log(evt.properties.cells)
+})
+
 export const editor = geditor
 
 export const graph = geditor.graph
 
+export const mxGraph = mxgraphModel.mxGraph
+
 export const mxRubberband = mxgraphModel.mxRubberband
 
 export const mxGraphHandler = mxgraphModel.mxGraphHandler
+
+export const mxEvent = mxgraphModel.mxEvent
+
+export const mxConstants = mxgraphModel.mxConstants
+
+export const mxUtils = mxgraphModel.mxUtils
